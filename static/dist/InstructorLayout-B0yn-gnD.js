97e3238c6996193d3db9614527f52ed5794fd731
wetende <cyprianwetende@gmail.com>
2026-08-21 00:15:34 +1000

    chore: rotate static distribution assets via build rebuild

--- static/dist/InstructorLayout-B0yn-gnD.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./vendor-motion-2Kw_RKO0.js";import{D as s}from"./index-C7k7Vz4A.js";function n({children:r,breadcrumbs:o=[]}){return t.jsx(s,{role:"instructor",breadcrumbs:o,children:r})}export{n as I};
